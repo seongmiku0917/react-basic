bfb5aa64596f564aaa1e57210c34ffad0994c0b0
seongminho <smiku0917@gmail.com>
2023-09-26 04:16:30 +0900

    Apply isLoading state

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(); // undefined 할당.
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
-
   const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined); // 초기화.
     fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
         setProducts(data);
-      });
+      })
+      .catch((e) => setError("에러가 발생했음!"))
+      .finally(() => setLoading(false));
     return () => {
-      console.log("🧹 꺠끗하게 청소하는 일들을 합니다.");
+      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
   }, [checked]);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
 
   return (
     <>
